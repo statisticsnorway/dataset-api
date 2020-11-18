abdfcd487090f0e214e6443628312ba951a665fb
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2020-11-18 12:37:02 +0100

    Remove unused module dependencies

--- src/main/java-mr/11/module-info.java
+++ src/main/java-mr/11/module-info.java
@@ -1,10 +1,5 @@
 module no.ssb.dapla.dataset.api {
-    requires com.google.common;
-    requires io.grpc;
-    requires grpc.stub;
     requires com.google.protobuf;
-    requires grpc.protobuf;
-    requires java.annotation;
 
     exports no.ssb.dapla.dataset.api;
     opens no.ssb.dapla.dataset.api;
